a0470431fd47f2430d6b3e46d20ee1f4f8c826ae
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-01-15 14:28:53 +0100

    :recycle: Refactors the location of google api url

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useRef } from 'react'
 
 const Map = ({ options, onMount, className }) => {
-  const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
-  const URL_GOOGLE_API = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`
   const ref = useRef()
 
   useEffect(() => {
+    const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+    const URL_GOOGLE_API = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`
     const onLoad = () => {
       const map = new window.google.maps.Map(ref.current, options)
       if (typeof onMount === `function`) onMount(map)
